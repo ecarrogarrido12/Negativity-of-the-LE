83d01223c71d2d1d15b788421b9f375ff8e298ff
Enrique Carro Garrido <ecarrogarrido12@gmail.com>
2026-09-21 16:42:05 +0200

    Updated case K = 1
    This simulations show that truncating in the first component gives us a negative integral!

--- bound_le.py
+++ bound_le.py
@@ -1,17 +1,17 @@
 import numpy as np
+from scipy.integrate import quad
 from scipy.sparse import diags
 from scipy.sparse.linalg import eigsh
-import matplotlib.pyplot as plt
-from concurrent.futures import ProcessPoolExecutor
+
 
 # Physical parameters
-alpha = 100 # Bifurcation parameter
+alpha = 50  # Bifurcation parameter
 L = 1  # Domain O = [0, L]
 
 # Numerical parameters
 N = 1000 # Number of grid points
-K = 10 # Fourier series truncation
-S = 100000 # MC samples
+K = 1  # Fourier series truncation
+R = 2 * np.sqrt(alpha)  # Truncation of the real line for the Lebesgue integral
 x = np.linspace(0, L, N) # Spatial domain grid
 x_int = x[1:-1] # Interior points
 dx = x[1] - x[0] # Mesh size
@@ -22,7 +22,7 @@ e_1 = np.sqrt(2.0 / L) * np.sin(np.pi * x_int / L)
 
 # Orthonormal basis for H = L^2(O; R^2)
 def e_(k):
-    return np.sqrt(2.0 / L) * np.sin(k * np.pi * x_int / L)
+    return np.sqrt(2.0 / L) * np.sin(k * np.pi * x / L)
 
 # Eigenvalues of the orthonormal basis
 def lambda_(k):
@@ -30,29 +30,20 @@ def lambda_(k):
 
 # Sample f from N(0, (-Delta)^-1) measure
 def f_mc(xi):
-    k_vals = np.arange(1, K + 1)
-    sqrt_lambda = k_vals * np.pi / L
-
-    # Pre-calculate the basis vectors e_(k) into a matrix of shape (K, N-2)
-    e_mat = np.sqrt(2.0 / L) * np.sin(np.outer(k_vals, np.pi * x_int / L))
-
-    # Scale xi by sqrt(lambda) and multiply by the basis matrix
-    sample = np.zeros((2, N - 2))
-    sample[0] = (xi[:, 0] / sqrt_lambda) @ e_mat
-    sample[1] = (xi[:, 1] / sqrt_lambda) @ e_mat
+    sample = np.zeros((2, len(x)))
+    for k in range(1, K + 1):
+        sample[0] += (xi[k - 1][0] / np.sqrt(lambda_(k))) * e_(k)
+        sample[1] += (xi[k - 1][1] / np.sqrt(lambda_(k))) * e_(k)
     return sample
 
-def potential(xi):
-    f = f_mc(xi)
-    f_squared = f[0]**2 + f[1]**2
-    l2_squared = 0.0
-    for k in range(1, K + 1):
-        l2_squared += (xi[k - 1][0]**2 + xi[k - 1][1]**2)/lambda_(k)
-    l4_forth = np.trapezoid(f_squared**2, x_int)
-    return -alpha / 2 * l2_squared + 1 / 4 * l4_forth
+def potential(r):
+    return (
+            -alpha / (2 * lambda_1) * r ** 2
+            + 3 / (8 * L * lambda_1 ** 2) * r ** 4
+    )
 
 
-def le_schrodinger_op(xi):
+def le_schrodinger_op(r):
     # Finite difference Laplacian
     main = -2 * np.ones(N - 2)
     off = np.ones(N - 3)
@@ -63,29 +54,34 @@ def le_schrodinger_op(xi):
     ) / dx ** 2
 
     # Schrodinger operator
-    f = f_mc(xi)
-    f_squared = f[0]**2 + f[1]**2
+    f_squared = (r ** 2 / lambda_1) * e_1 ** 2
     l_f = delta + diags(alpha - f_squared, 0)
 
     # Compute the largest eigenvalue of L_f
     return eigsh(l_f, k=1, which='LA', return_eigenvectors=False)[0]
 
-def one_sample(seed):
-    rng = np.random.default_rng(seed)
-    xi_array = np.random.uniform(-500, 500, (K, 2))
-    le = le_schrodinger_op(xi_array)
-    log_weight = (- 2 * potential(xi_array))
-    return le, log_weight
+
+# Integrands for the expectation
+def numerator_integrand(r):
+    return le_schrodinger_op(r) * np.exp(-0.5 * r ** 2 - 2 * potential(r))* r
+
+def denominator_integrand(r):
+    return np.exp(-0.5 * r**2 - 2 * potential(r)) * r
 
 if __name__ == "__main__":
-    seeds = np.random.SeedSequence(12345).spawn(S)
-    with ProcessPoolExecutor() as executor:
-        results = list(executor.map(one_sample, seeds))
-    les, log_weights = np.array(results).T
-    log_weights -= np.max(log_weights)
-    weights = np.exp(log_weights)
-    expectation = (np.sum(weights * les)/ np.sum(weights))
+    numerator = quad(
+        numerator_integrand,
+        16,
+        19,
+        limit=200
+    )[0]
+
+    denominator = quad(
+        denominator_integrand,
+        16,
+        19,
+        limit=200
+    )[0]
+
+    expectation = numerator / denominator
     print(expectation)
-
-
-
